cf728e0168fb75ac593a3633d4d0e17a4ef5f5ae
sebastianluszczek <luszczeksebastian@gmail.com>
2019-01-03 21:42:45 +0100

    +config keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://utkow1991:utkow_1991@cluster0-ympqd.mongodb.net/article_base_dev?retryWrites=true',
+    googleClientID: '440840016758-ig9uq5b05oigo88tmb7sk7o0hg1so91b.apps.googleusercontent.com',
+    googleClientSecret: 'XlSkBF_NI4TBj_mJet9WfapJ'
+}
